0b68a181409e8d5fe79afa47ff3262f1564beb25
Bingzheng Feng <b26feng@uwaterloo.ca>
2020-02-08 23:22:50 -0500

    File name check and int check works

--- weeder.cpp
+++ weeder.cpp
@@ -14,7 +14,142 @@ Weeder::Weeder(const std::string& file_name, const Token& parse_tree):
   class_name = slash_vec[slash_vec.size()-1];
   // Get rid of .java 
   class_name.erase(class_name.end()-5,class_name.end());
+  // Shrink tree
+  shrink(m_parse_tree);
   
+}
+
+
+void Weeder::shrink(Token& node){
+  // Shrink tree
+  while(node.m_generated_tokens.size() == 1){
+    node = node.m_generated_tokens[0];
+  }
+  for(Token& t:node.m_generated_tokens){
+    shrink(t);
+  }
+}
+
+bool Weeder::search(Token& node, TokenType key){
+  bool result = false;
+  if(node.m_type!=key){
+    for(Token& t:node.m_generated_tokens){
+      result = search(t,key);
+      if(result) break;
+    }
+  }else{
+    result = true;
+  }
+  return result;
+}
+
+bool Weeder::weed(Token& node,std::map<TokenType,int>& conditions){
+
+  std::istringstream iss;
+  std::map<TokenType,int> sub_cond = conditions;
+  /*
+  std::cout<<"-----------------------"<<std::endl;
+  if(conditions.find(TokenType::T_MINUS)==conditions.end()){
+    RED();
+    std::cout<<"NO ENTRY"<<std::endl;
+  }else{
+    std::cout<<conditions[TokenType::T_MINUS]<<std::endl;
+  }
+  */
+  
+  // Check before loop
+  //int type_n = static_cast<int>(node.m_type);
+  switch(node.m_type){
+  case TokenType::ClassDeclaration:
+    // Search for public key word in modifiers, if found check class name
+    if(search(node.m_generated_tokens[0],TokenType::T_PUBLIC)){
+      if(node.m_generated_tokens[2].m_lex.compare(class_name)){
+	  RED();
+	  std::cerr<<"WEEDER ERROR: class name and file name doesn't match!";
+	  std::cerr<<std::endl;
+	  DEFAULT();
+	  return false;
+	}
+    }
+    break;
+    
+  case TokenType::INT_LITERAL:
+    // Check int range;
+    if(node.m_lex.length() > 10){
+      RED();
+      std::cerr<<"WEEDER ERROR: int "<<node.m_lex<<" out of range.";
+      std::cerr<<std::endl;
+      DEFAULT();
+      return false;
+    }
+
+    iss.str(node.m_lex);
+    unsigned int large,threshold;
+    // Threshold differs for positive and negative numbers
+    if(conditions.find(TokenType::T_MINUS)!=conditions.end() &&
+       conditions[TokenType::T_MINUS] == 1){
+      threshold = 1<<31;
+    } else threshold = (1<<31)-1;
+			 
+    iss>>large;
+    if(large > threshold){
+      RED();
+      std::cerr<<"WEEDER ERROR: int "<<node.m_lex<<" out of range.";
+      std::cerr<<std::endl;
+      DEFAULT();
+      return false;
+    }
+    break;
+    
+  case TokenType::UnaryExpression:
+    //std::cout<<"UNARY:";
+    // If unary expression, add the entry into sub_cond
+    if(conditions.find(TokenType::T_MINUS)==conditions.end()){
+      //std::cout<<" Add Entry";
+      sub_cond[TokenType::T_MINUS] = 0;
+    }
+    //std::cout<<std::endl;
+    break;
+
+  case TokenType::T_PLUS:
+    //std::cout<<"T_PLUS"<<std::endl;
+    // If this is unary plus, set the flag to true
+    if(conditions.find(TokenType::T_MINUS)!= conditions.end()){
+      //std::cout<<": UNARY, to true";
+      conditions[TokenType::T_MINUS] = 0;
+    }
+    //std::cout<<std::endl;
+    break;
+  case TokenType::T_MINUS:
+    // If this is unary plus, set the flag to true
+    //std::cout<<"T_MINUS:";
+    if(conditions.find(TokenType::T_MINUS)!= conditions.end()){
+      // std::cout<<" Unary, to false";
+      conditions[TokenType::T_MINUS] = 1;
+    }
+    //std::cout<<std::endl;
+    break;
+  case TokenType::PrimaryNoNewArray:
+    // Check for bracketed components and reset plus minus flag
+    if(node.m_generated_tokens.size()>1){
+      sub_cond[TokenType::T_MINUS] = 0;
+    }
+    break;
+  default:
+    break;
+  }
+  //DEFAULT();
+  //std::cout<<"---------------------"<<std::endl;
+  
+
+  
+  
+  // Recursive call
+  for(Token& t: node.m_generated_tokens){
+    if(!weed(t,sub_cond)) return false;
+  }
+  
+  return true;
 }
 
 void Weeder::split(const std::string& s, const char delim, std::vector<std::string>& lst){
@@ -25,33 +160,8 @@ void Weeder::split(const std::string& s, const char delim, std::vector<std::stri
   }
 }
 
-bool Weeder::weed(Token& node){
-
-  // Check before loop
-  //int type_n = static_cast<int>(node.m_type);
-  switch(node.m_type){
-  case TokenType::ClassDeclaration:
-    // Check if the file name matches source
-    if(node.m_generated_tokens[2].m_lex.compare(class_name)){
-      RED();
-      std::cerr<<"WEEDER ERROR: class name and file name doesn't match!";
-      std::cerr<<std::endl;
-      DEFAULT();
-      return false;
-    }
-    break;
-  default:
-    break;
-  }
-
-  // Recursive call
-  for(Token& t: node.m_generated_tokens){
-    if(!weed(t)) return false;
-  }
-  
-  return true;
-}
 
 bool Weeder::weed(){
-  return weed(m_parse_tree);
+  std::map<TokenType,int> cond_table;
+  return weed(m_parse_tree,cond_table);
 }
--- weeder.h
+++ weeder.h
@@ -4,14 +4,15 @@
 
 #include <string>
 #include <vector>
-
+#include <map>
 
 class Weeder{
   std::string class_name;
   Token m_parse_tree;
 
-  
-  bool weed(Token& node);
+  void shrink(Token& node);
+  bool search(Token& node, TokenType key);
+  bool weed(Token& node,std::map<TokenType,int>& conditions);
   void split(const std::string& s, char delim, std::vector<std::string>& lst);
  public:
   Weeder(const std::string& file_name, const Token& parse_tree);
